f4a2107bf795a6cb730604530a74ca913476a2d3
ChristianTW <christian.tyler.w@gmail.com>
2019-10-15 00:33:19 -0700

    Initial Commit

---
+++ MacOS Game/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MacOS Game
+//
+//  Created by Christian Wegman on 10/15/19.
+//  Copyright © 2019 Techilepsy, LLC. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
